e141730c4c8309034efc4f95356f251cd72b0a42
Walther Enriquer Alcocer Gutierrez <walcocer.1982@gmail.com>
2024-07-24 22:40:31 -0500

    Agregado bucle

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
   // Preguntamos si desean que nos comuniquemos con ellos
-  let tipoMaquinaria = prompt("¿Qué tipo de maquinaria deseas alquilar");
-  let numeroMaquinaria = prompt("¿Cuántos equipos necesitas?");
+  function rentaEquipos(){  
+      let tipoMaquinaria = prompt("¿Qué tipo de maquinaria deseas alquilar");
+  
+  while (tipoMaquinaria !== "excavadora" && tipoMaquinaria !== "retroexcavadora"){
+    alert("No se tiene ese tipo de maquinaria");
+    rentaEquipos();
+  }
+  alert("Muchas gracias, su solictud de " + tipoMaquinaria + " está siendo procesada" );
+  }
+  function impresionImagen(){
+    let numeroMaquinaria = prompt("¿Cuántos equipos deseas?");
+    numeroMaquinaria = parseInt (numeroMaquinaria);
+    for(i = 0; i < numeroMaquinaria; i++){
+        document.write(
+         `<img src="https://static.unimaq.com.pe/fcsaprdunimaq01/2019/07/Retroexcavadoras.png" class="equipos-img" style = "width:360px; margin:40px">`+ (i + 1)
+        
+        ); 
+    }
+    
+  }
+  rentaEquipos();  
+  impresionImagen();
+ 
+
+//   let numeroMaquinaria = prompt("¿Cuántos equipos necesitas?");
   
 
-  function rentaEquipos(tipoMaquinaria, numeroMaquinaria){
-    if(tipoMaquinaria == "excavadora"){
-        alert("El alquiler de la excavadora está 95 dolares la hora");
-        if( numeroMaquinaria > 2){
-            alert("Lo sentimos no tenemos el stock disponible")
-        }else{
-            let telefono = prompt("Déjanos tu número de teléfono:");
-            alert("Nos comunicaremos al " + telefono + ". Gracias");
-        }
-    }
-    else if(tipoMaquinaria == "retroexcavadora"){
-        alert("El alquiler de la retroexcavadora está 70 dolares la hora");
-        if( numeroMaquinaria > 3){
-            alert("Lo sentimos no tenemos el stock disponible")
-        }else{
-            let telefono = prompt("Déjanos tu número de teléfono:");
-            alert("Nos comunicaremos al " + telefono + ". Gracias");
-        }
-    }else{
-        alert("Lo sentimos no tenemos ese tipo equipo. Gracias")
-    }
-  }
-  rentaEquipos(tipoMaquinaria, numeroMaquinaria);
+//   function rentaEquipos(tipoMaquinaria, numeroMaquinaria){
+//     if(tipoMaquinaria == "excavadora"){
+//         alert("El alquiler de la excavadora está 95 dolares la hora");
+//         if( numeroMaquinaria > 2){
+//             alert("Lo sentimos no tenemos el stock disponible")
+//         }else{
+//             let telefono = prompt("Déjanos tu número de teléfono:");
+//             alert("Nos comunicaremos al " + telefono + ". Gracias");
+//         }
+//     }
+//     else if(tipoMaquinaria == "retroexcavadora"){
+//         alert("El alquiler de la retroexcavadora está 70 dolares la hora");
+//         if( numeroMaquinaria > 3){
+//             alert("Lo sentimos no tenemos el stock disponible")
+//         }else{
+//             let telefono = prompt("Déjanos tu número de teléfono:");
+//             alert("Nos comunicaremos al " + telefono + ". Gracias");
+//         }
+//     }else{
+//         alert("Lo sentimos no tenemos ese tipo equipo. Gracias")
+//     }
+//   }
   
+
 //   if (interesado) {
 //     // Recopilamos el número de teléfono y correo electrónico
     
